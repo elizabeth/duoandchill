be8d718ad58b462eea9edcfda7d978bb39e9c699
Elizabeth Duong <johndoe@example.com>
2015-10-18 05:45:37 -0700

    Added styling

--- js/map.js
+++ js/map.js
@@ -2,9 +2,23 @@ $(function() {
 	'use strict';
 	var locationLat = 0;
 	var locationLong = 0;
+    var zoomLevel = parseInt("13");
 
 	$(".container").hide();
 	getLocation();
+
+    $("#default").click(function() {
+        zoomLevel = parseInt("13");
+        $("#level").val("1.5");
+        createMap();    
+    });
+
+    $("#level").change(function() {
+        if ($("#level").val() == "1.5") {
+            zoomLevel = parseInt("13");
+        }
+        createMap();
+    });
 
 //	$(window).resize(function() {
 //		var mapHeight = window.innerHeight * 0.5;
@@ -31,8 +45,14 @@ function setPosition(position) {
 }
 
 function createMap() {
+    $("#map").empty();
 	var map = new Microsoft.Maps.Map(document.getElementById('map'), {credentials: 'AgnpgjJOhsPeO0tYd2JJZMYnwtIKjPnjkZuGOfPHIMZeEGbDxRq-ZLgAnl98icuL', 
-	center: new Microsoft.Maps.Location(locationLat, locationLong), zoom: 15, disableZooming: true, showDashboard: false, enableSearchLogo: false, width: 500, height: 500});
+	center: new Microsoft.Maps.Location(locationLat, locationLong), zoom: 13, disableZooming: true, showDashboard: false, enableSearchLogo: false, width: 500, height: 500});
+    
+    map.entities.clear(); 
+    var pushpinOptions = {icon: 'http://ddragon.leagueoflegends.com/cdn/5.20.1/img/profileicon/937.png', width: 70, height: 70}; 
+    var pushpin= new Microsoft.Maps.Pushpin(map.getCenter(), pushpinOptions);
+    map.entities.push(pushpin);
 }
 
 function showError(error) {
