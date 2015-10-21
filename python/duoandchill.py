7cab6d764991521f93dfdcc9e4e718a433bdd7e1
luludust <johndoe@example.com>
2015-10-21 10:59:56 -0700

    Updated for testing.

--- python/duoandchill.py
+++ python/duoandchill.py
@@ -53,7 +53,8 @@ def main_loop():
 
 if __name__ == '__main__':
     try:
-        main_loop()
+	print "working blah blah test"
+	main_loop()
     except KeyboardInterrupt:
         print >> sys.stderr, '\nExiting by user request.\n'
         sys.exit(0)
